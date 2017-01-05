25692381b56efb60bdad5d83667816b6f53af08a
Jbob <contact@jbob.io>
2017-01-05 10:58:28 -0600

    Use a sails injector for plugin loading

--- api/services/PluginService.js
+++ api/services/PluginService.js
@@ -2,9 +2,10 @@ var path = require('path');
 var fs = require('fs');
 var rmdir = require('rimraf');
 var copy = require('recursive-copy');
-var pluginDir = path.join(__dirname, '../../plugins');
-var apiDir = path.join(__dirname, '../');
-var pluginConfigFile = 'package.json';
+var pluginDir = path.join(__dirname, '../../node_modules');
+var pluginConfig = path.join(__dirname, '../../plugins.json');
+var pluginPrefix = "fog-plugin-";
+var pluginPackageFile = 'package.json';
 var plugins = [];
 
 var resolveFunction = function(name) {
@@ -24,7 +25,7 @@ module.exports = {
   },
   registerHooks: function(pluginName, next) {
     sails.log.info("Registering [" + pluginName + "] hooks");
-    var configFile = path.join(pluginDir, pluginName, pluginConfigFile);
+    var configFile = path.join(pluginDir, pluginPrefix + pluginName, pluginPackageFile);
     fs.exists(configFile, function(exists) {
       if (!exists) {
         next("Plugin config file does not exist");
@@ -40,9 +41,9 @@ module.exports = {
         }
 
         var config = JSON.parse(data);
-        var hookKeys = Object.keys(config.hooks);
+        var hookKeys = Object.keys(config.fog.hooks);
         async.forEach(hookKeys, function(key, cb) {
-          var fnName = config.hooks[key].trim();
+          var fnName = config.fog.hooks[key].trim();
           if (!fnName) {
             sails.log.error("No function name provided when register [" + key + "] for [" + pluginName + "]");
             cb();
@@ -64,106 +65,41 @@ module.exports = {
       });
     });
   },
-  registerAPI: function(pluginName, next) {
-    sails.log.info("Registering [" + pluginName + "] API");
-    async.parallel([
-      function(callback) {
-        copy(path.join(pluginDir, pluginName, "controllers"), path.join(apiDir, "controllers", pluginName), function (err, results) {
-          callback();
-        });
-      },
-      function(callback) {
-        copy(path.join(pluginDir, pluginName, "models"), path.join(apiDir, "models", pluginName), function (err, results) {
-          callback();
-        });
-      },
-      function(callback) {
-        copy(path.join(pluginDir, pluginName, "policies"), path.join(apiDir, "policies", pluginName), function (err, results) {
-          callback();
-        });
-      },
-      function(callback) {
-        copy(path.join(pluginDir, pluginName, "responses"), path.join(apiDir, "responses", pluginName), function (err, results) {
-          callback();
-        });
-      },
-      function(callback) {
-        copy(path.join(pluginDir, pluginName, "services"), path.join(apiDir, "services", pluginName), function (err, results) {
-          callback();
-        });
-      },          
-    ], function(err, results) {
-      next(err);
-    });
-  },
-  removeAPI: function(pluginName, next) {
-    sails.log.info("Removing [" + pluginName + "] API");
-    async.parallel([
-      function(callback) {
-        rmdir(path.join(apiDir, "controllers", pluginName), function (err) {
-          callback();
-        });
-      },
-      function(callback) {
-        rmdir(path.join(apiDir, "models", pluginName), function (err) {
-          callback();
-        });
-      },
-      function(callback) {
-        rmdir(path.join(apiDir, "policies", pluginName), function (err) {
-          callback();
-        });
-      },     
-      function(callback) {
-        rmdir(path.join(apiDir, "responses", pluginName), function (err) {
-          callback();
-        });
-      },
-      function(callback) {
-        rmdir(path.join(apiDir, "services", pluginName), function (err) {
-          callback();
-        });
-      },            
-    ], function(err, results) {
-      next(err);
-    });
-  },
   scanPlugins: function(next) {
-    sails.log.info('Scanning plugin directory');
-    fs.readdir(pluginDir, function(err, files) {
-      async.forEach(files, function(file, cb) {
-        fs.stat(path.join(pluginDir, file), function(err, stats) {
-          if (err) {
-            cb();
-            return;
-          }
-
-          if (stats.isDirectory()) {
-            sails.log.info('Found Plugin [' + file + ']');
-            plugins.push(file);
-          }
-          cb();
-        });
-      }, function(err) {
+    sails.log.info('Scanning plugins');
+    fs.exists(pluginConfig, function(exists) {
+      if (!exists) {
+        next("Plugin config file does not exist, creating");
+        var configData = {
+          enabled: [],
+          disabled: []
+        };
+        var json = JSON.stringify(configData);
+        fs.writeFile(pluginConfig, json, 'utf8', next);
+        return;
+      }
+      
+      // Do not use require as it will execute any code
+      // Allowing for a potential attack vetor
+      fs.readFile(pluginConfig, 'utf8', function(err, data) {
+        if (err) {
           next(err);
+          return;
+        }
+
+        var config = JSON.parse(data);
+        plugins = config.enabled;
+        for (var i = 0, len = plugins.length; i < len; i++) {
+          sails.log.info("Found plugin [" + plugins[i] + "]");
+        }
+        next();
       });
     });
   },
-  cleanup: function(next) {
-    sails.log.info('Cleaning up leftover plugins');
-    next();
-  },
-  repair: function(next) {
-    sails.log.info('Repairing broken plugins');
-    next();
-  },
   initialize: function(next) {
     sails.log.info('Initializing Plugin System');
-    sails.log.info('Plugin Directory: "' + pluginDir + '"');
     async.waterfall([
-      PluginService.scanPlugins,
-      PluginService.cleanup,
-      PluginService.repair,
+      PluginService.scanPlugins,   
       function(callback) {
         async.forEach(plugins, function(plugin, cb) {
           PluginService.registerHooks(plugin, function(err) {
@@ -172,7 +108,7 @@ module.exports = {
         }, function(err) {
           callback(err);
         });       
-      }
+      },
     ], next);
   }
 };
